c97a8a03c5d431cae26750f9b5093fa8c080a852
Galis <galis.travelling@gmail.com>
2024-03-01 10:17:33 +0100

    DogCard and models

---
+++ src/components/DogCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="q-pa-sm">
+    <div class="shadow-5 rounded-borders">
+      <q-card flat>
+        <div class="q-pa-sm">
+          <q-img :src="image" class="rounded-borders" ratio="1" width="300px" />
+        </div>
+        <q-card-section>
+          <div class="text-body2">{{ breed }}</div>
+          <div class="text-caption">{{ color }}</div>
+          <div class="text-caption">{{ size }}</div>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { DogInfo } from './models';
+
+defineProps<DogInfo>();
+</script>
+
+<style scoped></style>
